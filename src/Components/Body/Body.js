7bb2a9379a591b1caa21038cb7ba18eaae896316
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-07-27 18:30:23 +0530

    Data Updated with DOJ, DOL and Salary

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { Employees } from "../../Constants/Employees";
 import "./Body.css";
 const Body = () => {
 
+  async function getEmployees() {
+    //fetching data from API
+    const data = await fetch(
+      `${Employees}`
+    );
+    const json = await data.json();
+    console.log("Json: ", json);
+  }
 
+  useEffect(()=>{
+    getEmployees();
+  },[])
+  
   return (
     <>
-      
+      Ayush
     </>
   );
 };
