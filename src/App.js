88b1b2f62f13b0c050b9c89b4311dc08287ee01a
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-07-27 18:05:56 +0530

    project inilialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import "./App.css";
+import Body from "./Components/Body/Body";
+
+function App() {
+  return (
+    <Body />
+  );
+}
+
+export default App;
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import "./Body.css";
+const Body = () => {
+
+
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Body;
